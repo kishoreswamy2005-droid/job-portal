a41d33117975e8e3948042786f627adb4aac9b98
Kishore <kishore2606k@gmail.com>
2026-05-02 15:38:03 +0530

    Configure for GitHub Pages and Render deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'https://kishoreswamy2005-droid.github.io'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
